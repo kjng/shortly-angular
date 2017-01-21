433abe32a478450986a67e9b4f4aab528362cbac
Kevin Jang <pianistkevinjang@gmail.com>
2017-01-21 12:12:48 -0800

    Fix navbar and redirect after shortening

--- client/app/app.js
+++ client/app/app.js
@@ -22,6 +22,10 @@ angular.module('shortly', [
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
+    })
+    .otherwise({
+      templateUrl: '/app/links/links.html',
+      controller: 'LinksController'
     });
 
     // We add our $httpInterceptor into the array
@@ -54,6 +58,7 @@ angular.module('shortly', [
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+    console.log('Next: ' + next);
     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
       $location.path('/signin');
     }
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,9 +2,12 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.url = '';
-  $scope.link = { url: $scope.url };
+  $scope.link = {};
   $scope.addLink = function() {
-    Links.addOne($scope.link);
-    $location.path = '/links';
+    $scope.link.url = $scope.url;
+    Links.addOne($scope.link)
+    .then(function(link) {
+      $location.url('/links');
+    });
   };
 });
